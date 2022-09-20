004f14a67e2ba99a1828428dbeb8a1bcd10f7cd9
Eugen 725 <ev_grigorenko_k@mail.ru>
2022-09-20 13:30:55 +0300

    Homework9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,86 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+/*
+Console.Write("Введите число m: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число n: "); 
+int n = Convert.ToInt32(Console.ReadLine());  
+
+void PrintInterval(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		PrintInterval(m, n);
+		
+		Console.WriteLine();
+
+
+//Задайте значения M и N. Напишите программу,
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма сумму натуральных элементов в промежутке от M до N= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+*/
